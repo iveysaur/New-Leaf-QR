6bac096f251154aab4295f3a27c1ab48e2016a1f
ivey <iveysaurrr@gmail.com>
2015-05-10 14:42:47 -0400

    Add routes for the different pages

--- backend.py
+++ backend.py
@@ -2,9 +2,33 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
+@app.route('/')
+def index():
     return render_template('index.html')
+
+@app.route('/dresses')
+def dresses():
+    return 'dresses'
+
+@app.route('/patterns')
+def patterns():
+    return 'patterns'
+
+@app.route('/shirts')
+def shirts():
+    return 'shirts'
+
+@app.route('/cutouts')
+def cutouts():
+    return 'cutouts'
+
+@app.route('/hats')
+def hats():
+    return 'hats'
+
+@app.route('/paths')
+def paths():
+    return 'paths'
 
 if __name__ == "__main__":
     app.run()
